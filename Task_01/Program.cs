0a4e1b55e93d6ad10ae11462b1af3ad6e075ff8e
SergeyMavrin <mavrin_sergei@list.ru>
2022-06-23 20:14:04 +0400

    Task01

---
+++ Task_01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа
+//и проверяет, является ли первое чилсо квадратом второго
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 = num2 * num2) {
+    Console.Write($"Число {num1} является квадратом {num2}");
+}
+else {
+    Console.Write($"Число {num1} не является квадратом {num2}");
+}
