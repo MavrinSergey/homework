8a1b496397019e677a52a1a63d253ca7793b15f9
SergeyMavrin <mavrin_sergei@list.ru>
2022-06-30 20:58:58 +0400

    Task 22

--- Task_22/Program.cs
+++ Task_22/Program.cs
@@ -12,6 +12,20 @@ int square = 0;
 while (count <= num)
 {
     square = count *count;
-    Console.WriteLine($"{count} -> {square}");   
+    Console.WriteLine($"{count, 3} -> {square, 5}");   
     count++;
 }
+
+//через массив
+Console.WriteLine ("Программа запрашивает число N и выводит таблицу квадратов чисел");
+Console.WriteLine ("Введите число N: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int i = 1;
+int[] tableq = new int [n];
+
+while (i <= n)
+{
+tableq[i - 1] = i * i;
+i++;
+}
+Console.WriteLine ($"Таблица квадратов чисел от 1 до {n}: {String.Join(", ",tableq)}");
