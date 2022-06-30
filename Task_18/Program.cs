75cb0f745fa289a634361357aed9679bffb04b46
SergeyMavrin <mavrin_sergei@list.ru>
2022-06-30 19:16:50 +0400

    Task 18

--- Task_18/Program.cs
+++ Task_18/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 18.Напишите программу которая по заданному номеру четверти
+//показывает диапазон возможных координат точек в этой четверти (x и y)
+
+Console.WriteLine("Введите порядковый номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string Coord(int q)
+{
+    switch (q)
+    {
+        case 1:
+            return "Диапазон возможных координат x > 0 и y > 0";
+        case 2:
+            return "Диапазон возможных координат x < 0 и y > 0";
+        case 3:
+            return "Диапазон возможных координат x < 0 и y < 0";
+        case 4:
+            return "Диапазон возможных координат x > 0 и y < 0";
+        default:
+            return "Введена не корректная четверть";
+    }
+}
+
+string result = Coord(quarter);
+Console.WriteLine(result);
