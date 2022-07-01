58e37eadd593d38eda1367405467b4ed4392c32e
SergeyMavrin <mavrin_sergei@list.ru>
2022-07-01 13:08:34 +0400

    Task 19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -4,9 +4,24 @@
 //12821 -> да
 //23432 -> да
 
-Console.WriteLine("Введите пятизначное число ");
+Console.WriteLine("Программа показывает является ли введенное число полиндромом.");
+Console.Write("Введите пятизначное число ");
 int num = Convert.ToInt32(Console.ReadLine());
-while (num < 10000 || num > 9999) {
-    Console.Write("Введено не пятизначное число. Попробуйте еще раз ");
-        num = Convert.ToInt32(Console.ReadLine());
+bool Palindrome(int n)
+{
+    while (n < 10000 || n > 99999)
+    {
+        Console.Write("Введено не пятизначное число. Попробуйте еще раз ");
+        n = Convert.ToInt32(Console.ReadLine());
     }
+    if (n / 10000 == n % 10 || n / 1000 == num % 100)
+    {
+        return true;
+    }
+    else return false;
+}
+if (Palindrome(num) == true)
+{
+    Console.WriteLine($"Введенной число {num} полиндром");
+}
+else Console.WriteLine($"Введенной число {num} не полиндром");
