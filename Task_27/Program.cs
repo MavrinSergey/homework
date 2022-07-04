a8b44cfeedc43f869e37839f2ea4ba47db28f37a
SergeyMavrin <mavrin_sergei@list.ru>
2022-07-04 22:13:59 +0400

    Task27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -2,3 +2,25 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int DigitsCount(int num)
+{
+    int count = 0;
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+        count++;
+    }
+    return sum;
+}
+int res = DigitsCount(a); //21-25 строка дополнение.
+while (res > 9)// при получении двухзначного res мы сново применим к нему функцию, чтобы результат был однозначным
+               //Трехзначное число получится не сможет так как не даст int ввести такое число
+{
+    res = DigitsCount(res);
+}
+System.Console.WriteLine(res);
