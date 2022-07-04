a807fe0e2e39d0621b46803ad78c510a2d24b814
SergeyMavrin <mavrin_sergei@list.ru>
2022-07-04 16:45:32 +0400

    Task 28

--- Task_28/Program.cs
+++ Task_28/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 28: Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        sum = sum * i;
+    }
+   return sum;
+}
+int res = Sum(a);
+System.Console.WriteLine(res);
