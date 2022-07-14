9dd97e5420d2ba72242ce6f835ca6eb301844890
SergeyMavrin <mavrin_sergei@list.ru>
2022-07-14 20:46:31 +0400

    Task34

---
+++ Task_50/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) System.Console.Write("[ ");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}");
+            else System.Console.Write($"{array[i, j],3}   ]");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SearchCellValues(int[,] array, int row, int col)
+{
+    int answer = array[row - 1, col - 1];//нумерация столбцов и строк с 1.
+    return answer;
+}
+
+
+int[,] table = CreateMatrixRndInt(4, 4, 0, 9);
+PrintMatrix(table);
+Console.WriteLine("Программа находит значние ячейки данной таблицы");
+Console.Write("Введите номер строки нужной вам ячейки: ");
+int RowPositionCell = Convert.ToInt32(Console.ReadLine());
+while (RowPositionCell > table.GetLength(0))
+{
+    Console.Write("Такой строки в таблице нет, повторите ввод строки: ");
+    RowPositionCell = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Введите номер столбца нужной вам ячейки: ");
+int colPositionCell = Convert.ToInt32(Console.ReadLine());
+while (colPositionCell > table.GetLength(1))
+{
+    Console.Write("Такого столбца в таблице нет, повторите ввод столбца: ");
+    colPositionCell = Convert.ToInt32(Console.ReadLine());
+}
+int value = SearchCellValues(table, RowPositionCell, colPositionCell);
+System.Console.WriteLine(value);
---
+++ Task_52/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) System.Console.Write("[ ");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j]} ");
+            else System.Console.Write($"{array[i, j]} ]");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[] AverageCol(int[,] array)
+{
+    double[] averageColArr = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        averageColArr[j] = (double)sum / array.GetLength(0);
+    }
+    return averageColArr;
+}
+
+void PrintArray(double[] array) //печатает массив
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) System.Console.Write("[");
+        if (i < array.Length - 1) Console.Write(Math.Round(array[i], 2) + "; ");
+        else System.Console.Write(Math.Round(array[i], 2) + "]");
+    }
+}
+
+
+Console.WriteLine("Программа находит среднее арифметическое элементов в каждом столбце.");
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте нужный диапазон чисел.");
+Console.Write("Нижняя граница диапазона: ");
+int begin = Convert.ToInt32(Console.ReadLine());
+Console.Write("Верхняя граница диапазона: ");
+int end = Convert.ToInt32(Console.ReadLine());
+
+int[,] arrayResult = CreateMatrixRndInt(row, col, begin, end);
+PrintMatrix(arrayResult);
+double[] average = AverageCol(arrayResult);
+PrintArray(average);
