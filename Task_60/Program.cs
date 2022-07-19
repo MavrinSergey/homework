fb1980fa973c760e4291fafcad564fc699d34472
SergeyMavrin <mavrin_sergei@list.ru>
2022-07-19 19:54:20 +0400

    Task60

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -6,21 +6,48 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-
-
 Console.Clear();
-int[,,] CreateMatrix(int x, int y, int z)
+int[] InputData()
 {
-    int[,,] matrix = new int[x, y, z];
+    Console.WriteLine("Введите размерность массива.");
+    Console.Write("Введите размер оси x: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите размер оси y: ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите размер оси z: ");
+    int z = Convert.ToInt32(Console.ReadLine());
+    int[] array = { x, y, z };
+    return array;
+}
+
+int[] CreatArray(int[] array)
+{
+    int[] temp = new int[array[0] * array[1] * array[2]];
     Random rnd = new Random();
-    int[] temp = new int[x * y * z];
+    int tempNum = 0;
+    for (int i = 0; i < temp.Length; i++)
+    {
+        tempNum = rnd.Next(10, 100);
+        while (temp.Contains(tempNum))
+        {
+            tempNum = rnd.Next(10, 100);
+        }
+        temp[i] = tempNum;
+    }
+    return temp;
+}
+
+int[,,] FilldMatrix(int[] array, int[] inputArray)
+{
+    int[,,] matrix = new int[inputArray[0], inputArray[1], inputArray[2]];
+    int counter = 0;
     for (int i = 0; i < matrix.GetLength(2); i++)
     {
         for (int j = 0; j < matrix.GetLength(0); j++)
         {
             for (int k = 0; k < matrix.GetLength(1); k++)
             {
-                matrix[j, k, i] = rnd.Next(10, 100);
+                matrix[j, k, i] = array[counter++];
             }
         }
     }
@@ -44,7 +71,9 @@ void PrintMatrix(int[,,] matrix)
 }
 
 Console.WriteLine("Программа создает трехмерный массив.");
-int[,,] myMatrix = CreateMatrix(2, 2, 2);
+int[] inputData = InputData();
+int[] myArray = CreatArray(inputData);
+int[,,] myMatrix = FilldMatrix(myArray, inputData);
 Console.WriteLine();
 PrintMatrix(myMatrix);
-// вводятся рандомные проверки на повторяющиеся нет
+// помоему программа некоректно работает если значения x,y,z ввести разные.
